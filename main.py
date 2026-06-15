3d684d610133b4b0bd9688321c5e8f7997c6ba7a
saradel22 <georgesharonsara3@gmail.com>
2026-06-15 22:27:16 +0530

    main.py

--- main.py
+++ main.py
@@ -1,99 +1,108 @@
-import csv
-import os
+from tkinter import *
+import math
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 1
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
-FILE_NAME = "volunteers.csv"
+# ---------------------------- TIMER RESET ------------------------------- # 
 
-# Create file if it doesn't exist
-if not os.path.exists(FILE_NAME):
-    with open(FILE_NAME, "w", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow(["ID", "Name", "Age", "Phone", "Skill"])
-
-
-def add_volunteer():
-    volunteer_id = input("Enter Volunteer ID: ")
-    name = input("Enter Name: ")
-    age = input("Enter Age: ")
-    phone = input("Enter Phone Number: ")
-    skill = input("Enter Skill: ")
-
-    with open(FILE_NAME, "a", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow([volunteer_id, name, age, phone, skill])
-
-    print("\nVolunteer Added Successfully!\n")
-
-
-def view_volunteers():
-    print("\n--- Volunteer List ---")
-
-    with open(FILE_NAME, "r") as file:
-        reader = csv.reader(file)
-
-        for row in reader:
-            print(row)
-
-    print()
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
 
 
-def search_volunteer():
-    search_name = input("Enter Volunteer Name: ").lower()
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-    found = False
+def start_timer():
+    global reps
+    reps += 1
 
-    with open(FILE_NAME, "r") as file:
-        reader = csv.reader(file)
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
 
-        for row in reader:
-            if len(row) > 1 and search_name in row[1].lower():
-                print(row)
-                found = True
-
-    if not found:
-        print("Volunteer Not Found")
-
-
-def generate_report():
-    count = 0
-
-    with open(FILE_NAME, "r") as file:
-        reader = csv.reader(file)
-
-        next(reader)
-
-        for row in reader:
-            count += 1
-
-    print("\n===== NayePankh Foundation Report =====")
-    print("Total Volunteers:", count)
-    print("=======================================\n")
-
-
-while True:
-    print("===== NayePankh Foundation =====")
-    print("1. Add Volunteer")
-    print("2. View Volunteers")
-    print("3. Search Volunteer")
-    print("4. Generate Report")
-    print("5. Exit")
-
-    choice = input("Enter Choice: ")
-
-    if choice == "1":
-        add_volunteer()
-
-    elif choice == "2":
-        view_volunteers()
-
-    elif choice == "3":
-        search_volunteer()
-
-    elif choice == "4":
-        generate_report()
-
-    elif choice == "5":
-        print("Thank You!")
-        break
-
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
     else:
-        print("Invalid Choice")
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text=marks)
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
+
+
+
+
+
+
+window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
